5f397af7a15462318a416abe958bb2d8e69420b5
BOSON-18 <deeshankbatra663@gmail.com>
2024-05-11 07:29:59 -0700

    done basic HW

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -1,114 +1,145 @@
 const Category = require("../models/Category");
 const User = require("../models/User");
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 //create a Category handler function
 
 exports.createCategory = async (req, res) => {
-    try {
-        //fetch data
-        const { name, description  } = req.body;
+  try {
+    //fetch data
+    const { name, description } = req.body;
 
-        //validation
-        if (!name || !description) {
-            return res.status(400).json({
-                success: false,
-                message: "All Fields are required",
-            })
-        }; 
-
-        
-
-        //create entry in db
-
-        const CategoryDetails = await Category.create({
-            name: name,
-            description: description,
-        });
-        console.log(CategoryDetails);
-
-        return res.status(200).json({
-            success: true,
-            message: "Category created successfully"
-        });
-
+    //validation
+    if (!name || !description) {
+      return res.status(400).json({
+        success: false,
+        message: "All Fields are required",
+      });
     }
-    catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: error.message,
-        })
-    }
+
+    //create entry in db
+
+    const CategoryDetails = await Category.create({
+      name: name,
+      description: description,
+    });
+    console.log(CategoryDetails);
+
+    return res.status(200).json({
+      success: true,
+      message: "Category created successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
-
 
 //getAllCategorys hndlerfuncion
 
 exports.showAllCategories = async (req, res) => {
-    try {
-        const allCategory = await Category.find({}, { name: true, description: true });//koi searching criteia nhi but make sure it contains name and description
+  try {
+    const allCategory = await Category.find(
+      {},
+      { name: true, description: true }
+    ); //koi searching criteia nhi but make sure it contains name and description
 
-        return res.json({
-            success: true,
-            message: "All Category returned successfully",
-            allCategory
-        });
-
-    }
-    catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: error.message,
-        })
-
-    }
-}
+    return res.json({
+      success: true,
+      message: "All Category returned successfully",
+      allCategory,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 //category pageDetails
 
 exports.categoryPageDetails = async (req, res) => {
-    try {
+  try {
+    //get category id
+    const { categoryId } = req.body;
+    //fetch all courses of the category
+    const selectedCategory = await Category.findById(categoryId)
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+        // populate:"ratingAndReviews"
+      })
+      .exec();
 
-        //get category id
-        const { categoryId } = req.body;
-        //fetch all courses of the category
-        const selectedCategory = await Category.findById(categoryId).populate("courses").exec();
-
-        //validation 
-        if (!selectedCategory) {
-
-            return res.status(404).json({
-                success: false,
-                message: "Course not found ",
-            });
-        }
-
-        //get course for different categories
-        const differentCategories = await Category.find(
-            {
-                _id: { $ne: categoryId },
-            }
-        ).populate("courses").exec();
-        //get topselling courses
-        //ye Hw hai
-
-        //return response
-        return res.status(200).json({
-            success: true,
-            data: {
-                selectedCategory,
-                differentCategories
-            }
-        });
-
+    //validation
+    if (!selectedCategory) {
+      return res.status(404).json({
+        success: false,
+        message: "Course not found ",
+      });
     }
-    catch (error) {
 
-        console.log(error);
+    if (selectedCategory.courses.length === 0) {
+      console.log("No COurses found for this cattegory");
 
-        return res.status(500).json({
-            success: false,
-            message: error.message
-        })
-
+      return res.status(404).json({
+        success: false,
+        message: "No courses found for the selected category",
+      });
     }
-}
+
+    //get course for different categories i.e all except selected
+    const categoriesExceptSelected = await Category.find({
+      _id: { $ne: categoryId },
+    });
+    let differentCategory = await Category.findOne(
+      categoriesExceptSelected[getRandomInt(categoriesExceptSelected.length)]
+        ._id
+    )
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+      })
+      .exec();
+    //get topselling courses
+    //ye Hw hai
+
+    //top selling courses across categories
+
+    const allCategories = await Category.find()
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+        populate: {
+          path: "instructor",
+        },
+      })
+      .exec();
+
+    //return response
+    const allCourses = allCategories.flatMap((category) => category.courses)
+      const mostSellingCourses = allCourses
+        .sort((a, b) => b.sold - a.sold)
+        .slice(0, 10)
+       // console.log("mostSellingCourses COURSE", mostSellingCourses)
+      res.status(200).json({
+        success: true,
+        data: {
+          selectedCategory,
+          differentCategory,
+          mostSellingCourses,
+        },
+      })
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
